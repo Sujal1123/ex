bc3aa9f46d3061c12be563a6bca7cdc72cd2e779
Sujal Dandhale <sujaldandhale94@gmail.com>
2024-10-12 15:18:04 +0530

    new changes.

--- app.js
+++ app.js
@@ -12,29 +12,8 @@ const User = require("./models/user.js");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://sujaldandhale:PgBoh6l72RNS5x9q@cluster1.tkzjs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/exp";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/exp";
 
 main().then(() => {
     console.log("connected to db");
@@ -43,7 +22,7 @@ main().then(() => {
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(uri);
 }
 
 app.set("views engine", "ejs");
